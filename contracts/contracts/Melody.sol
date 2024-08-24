fde25a3850d598af2ed4f312c4f9cd1b710954d3
skyh24 <skyhighfeng@gmail.com>
2024-08-24 15:07:03 +0800

    feat: solidity contract

---
+++ contracts/contracts/Melody.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.21;
+
+import {RMRKMultiAssetPreMint} from "@rmrk-team/evm-contracts/contracts/implementations/premint/RMRKMultiAssetPreMint.sol";
+import {RMRKAbstractMultiAsset} from "@rmrk-team/evm-contracts/contracts/implementations/abstract/RMRKAbstractMultiAsset.sol";
+
+
+contract Melody is RMRKMultiAssetPreMint {
+    // Events 
+    // Variables
+
+    // Constructor
+    constructor(
+          string memory collectionMetadata,
+          uint256 maxSupply,
+          address royaltyRecipient,
+          uint16 royaltyPercentageBps
+    )
+          RMRKMultiAssetPreMint(
+              "Melody",
+              "MLD",
+              collectionMetadata,
+              maxSupply,
+              royaltyRecipient,
+              royaltyPercentageBps
+          )
+      {}
+      
+    // Methods
+}
