63ce9588312f5e5f2c0b7bb1212458a21efc3c81
Fortitude-lan <358098351@qq.com>
2024-08-25 14:42:18 +0800

    connect

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = { reactStrictMode: true,
+    i18n: {
+      locales: ['en-US', 'zh-CN'],
+      defaultLocale: 'en-US',
+    },};
 
 export default nextConfig;
